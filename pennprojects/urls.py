9c635a999d49acef0e1730c1c062e0148a1ed725
Tom Shen <tomshen@cmu.edu>
2012-12-29 22:35:18 -0500

    I hope this works

--- pennprojects/urls.py
+++ pennprojects/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^projects/', include('projects.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^projects/', include('projects.urls')),
 )
