1541946a8791c1248986b137a81248fc3e80d8d4
Tom Shen <tomshen@cmu.edu>
2012-12-29 23:29:32 -0500

    Now with functional admin interface

--- projects/admin.py
+++ projects/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 class ProjectAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,              {'fields':['name']}),
-        ('Other information',{'fields':['sub_date'], 'classes':['collapse']}),
+        (None,              {'fields':['name', 'authors', 'sub_date', 'pitch']}),
+        ('Other information',{'fields':['description', 'album_url', 'source_url', 'demo_url'], 'classes':['collapse']}),
     ]
     list_display = ('name', 'authors', 'sub_date')
     list_filter = ['sub_date']
