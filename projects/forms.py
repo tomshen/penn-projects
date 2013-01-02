b4556c879b40d25978b7b8e37e2758dd96c7d222
Tom <tomshen@cmu.edu>
2013-01-02 00:15:09 -0500

    Made fields actually optional

--- projects/forms.py
+++ projects/forms.py
@@ -3,6 +3,13 @@ from projects.models import Project
 from django.utils import timezone
 
 class ProjectForm(ModelForm):
+
     class Meta:
         model = Project
         exclude = ('approved', )
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        self.fields['authors'].widget.attrs['rows'] = 3
+        self.fields['description'].widget.attrs['rows'] = 10
+        self.fields['pitch'].widget.attrs['rows'] = 3
--- projects/models.py
+++ projects/models.py
@@ -8,12 +8,12 @@ class Project(models.Model):
 	name = models.CharField('project name', max_length=50)
 	sub_date = models.DateTimeField('date submitted', editable=False)
 	authors = models.TextField(max_length=100)
-	demo_url = models.URLField(blank=True)
+	demo_url = models.URLField(blank=True, null=True)
 	description = models.TextField()
-	album_url = models.URLField(blank=True)
+	album_url = models.URLField(blank=True, null=True)
 	pitch = models.TextField(max_length=200)
-	source_url = models.URLField(blank=True)
-	thumbnail_url = models.URLField(blank=True)
+	source_url = models.URLField(blank=True, null=True)
+	thumbnail_url = models.URLField(blank=True, null=True)
 	approved = models.BooleanField(choices=APPROVAL)
 
 	def __unicode__(self):
